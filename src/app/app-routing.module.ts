6f74ac23509a2a3cf9ea703beba99c9c15738659
Nicolas Hidalgo <nicolas.hidalgo.1989@gmail.com>
2024-01-24 14:46:48 -0300

    feat: inicializacion modulos pages y rutas formApp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ReactiveModule } from './reactive/reactive.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: 'reactive',
+    loadChildren: () => import('./reactive/reactive.module').then( m => m.ReactiveModule ),
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then( m => m.AuthModule ),
+  },
+  {
+    path: '**',
+    redirectTo: 'reactive'
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
