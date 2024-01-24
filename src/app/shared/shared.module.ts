3eeb87952b9dfcb1d05ab2b46ebe615e2ba8ed41
Nicolas Hidalgo <nicolas.hidalgo.1989@gmail.com>
2024-01-24 15:21:31 -0300

    feat: sideMenu | reactive form FormBuilder

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,19 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
-
+import { SideMenuComponent } from './components/side-menu/side-menu.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SideMenuComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
+  ],
+  exports:[
+    SideMenuComponent
   ]
 })
 export class SharedModule { }
