eb7cee9ca41fe904ef4baf261d2a99be391f7c09
Nicolas Hidalgo <nicolas.hidalgo.1989@gmail.com>
2024-01-24 19:47:08 -0300

    feat: delete campos del formarray

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -53,6 +53,10 @@ export class DynamicPageComponent {
 
   }
 
+  onDeleteFavorite( index: number ): void{
+    this.favoriteGames.removeAt(index);
+  }
+
   onSubmit():void{
 
     if(this.myForm.invalid){
