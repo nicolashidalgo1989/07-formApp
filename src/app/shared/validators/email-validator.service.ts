1d214c5daef8b627809e61bfce01b63ff5f1405a
Nicolas Hidalgo <nicolas.hidalgo.1989@gmail.com>
2024-01-24 23:28:58 -0300

    feat: validate taken email

--- src/app/shared/validators/email-validator.service.ts
+++ src/app/shared/validators/email-validator.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { AbstractControl, AsyncValidator, ValidationErrors } from "@angular/forms";
-import { Observable, delay, of } from "rxjs";
+import { Observable, delay } from "rxjs";
 
 @Injectable({ providedIn: 'root' })
 export class EmailValidator implements AsyncValidator{
@@ -9,14 +9,42 @@ export class EmailValidator implements AsyncValidator{
 
     const email = control.value;
 
-    console.log(email);
+    const httpCallObservable = new Observable<ValidationErrors | null>( subscriber => {
 
-    return of({
-      emailTaken: true
+      console.log({ email });
+
+      if(email === 'nico@gmail.com'){
+
+        subscriber.next({ emailTaken: true });
+        subscriber.complete();
+        return;
+
+      }
+
+      subscriber.next(null);
+      subscriber.complete();
+
+
     }).pipe(
-      delay(2000)
+      delay( 3000 )
     )
 
+    return httpCallObservable;
+
   }
+
+  // validate(control: AbstractControl): Observable<ValidationErrors | null> {
+
+  //   const email = control.value;
+
+  //   console.log(email);
+
+  //   return of({
+  //     emailTaken: true
+  //   }).pipe(
+  //     delay(2000)
+  //   )
+
+  // }
 
 }
