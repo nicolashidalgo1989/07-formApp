d5d073881e5ed200942183a7bf49167e510b2cfe
Nicolas Hidalgo <nicolas.hidalgo.1989@gmail.com>
2024-01-24 22:01:01 -0300

    feat: validators y custom validator function

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,9 +1,33 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { cantBeStrider } from '../../../shared/validators/validators';
 
 @Component({
-  templateUrl: './register-page.component.html',
-  styles: ``
+  templateUrl: './register-page.component.html'
 })
 export class RegisterPageComponent {
+
+  public myForm: FormGroup = this.fb.group({
+    name: [ '', [Validators.required] ],
+    email: [ '', [Validators.required, Validators.email] ],
+    username: [ '', [Validators.required, cantBeStrider ] ],
+    password: [ '', [Validators.required, Validators.minLength(6) ] ],
+    password2: [ '', [Validators.required ] ]
+  })
+
+  constructor( private fb: FormBuilder){}
+
+  isValidField(field:string){
+    // todo obtener validacion desde un servicio
+  }
+
+  onSubmit(){
+    if(this.myForm.invalid){
+      this.myForm.markAllAsTouched();
+      return;
+    }
+    console.log(this.myForm.value);
+
+  }
 
 }
