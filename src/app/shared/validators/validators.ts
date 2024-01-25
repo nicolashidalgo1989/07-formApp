eb6287f3d6a56408133eb313621b00138602a2bd
Nicolas Hidalgo <nicolas.hidalgo.1989@gmail.com>
2024-01-24 22:18:41 -0300

    feat: validators patterns

--- src/app/shared/validators/validators.ts
+++ src/app/shared/validators/validators.ts
@@ -1,17 +1,14 @@
-import { FormControl } from "@angular/forms";
+import { FormControl, ValidationErrors } from "@angular/forms";
 
-export const cantBeStrider = ( control: FormControl ) => {
+export const firstNameAndLastnamePattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+export const emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
+
+export const cantBeStrider = ( control: FormControl ): ValidationErrors  | null=> {
 
   const value: string = control.value.trim().toLowerCase();
 
-  if( value === 'strider' ){
-
-      return{
-
-        noStrider: true,
-
-      }
-
+  if( value === 'strider' ) {
+    return{ noStrider: true }
   }
 
   return null;
